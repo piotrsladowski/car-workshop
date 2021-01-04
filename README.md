0c4c96cd24411cd5eddedddc0116e28d6e332c9c
piotrsladowski <piotrsladowski7@gmail.com>
2021-01-04 15:37:32 +0100

    added templated

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # car-workshop
+
+### Virtualenv 
+1. Get `virtualenv` using `pip3 install virtualenv`
+2. `cd <project-directory>`
+3. `virtualenv <envName>` 
+4. `.\<envName>\Scripts\activate`
+5. Install all required modules using `pip3 install -r requirements.txt`
+6. Run app by typing `python .\src\main.py`
---
+++ app/routes.py
@@ -0,0 +1,116 @@
+from flask import render_template, flash, redirect, request
+from app import app, db
+#from app.models import Course, Files
+import re
+import os
+from pathlib import Path
+import time
+import json
+
+# This line has to be set manually
+coursesList = []
+
+########### Logic #############
+
+def clear_console():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+def verbose_cls():
+    clear_console()
+
+def handle_windows_path(posix_path):
+    if os.name == 'nt':
+        return posix_path.replace('/', '\\')
+    return posix_path
+
+"""
+def getCourseList():
+    global coursesList
+    tempList = Course.query.all()
+    for item in tempList:
+        c = str(item).split()[1]
+        c = c[:-1]
+        coursesList.append(c)
+"""
+#getCourseList()
+data = [{
+  "name": "spojler_fiat_126p",
+  "description": "Spojler do malucha",
+  "amount": "122",
+  "price": "450"
+},
+ {
+  "name": "plandeka_zuk",
+  "description": "Plandeka od żuka",
+  "amount": "0",
+  "price": "123,23"
+}, {
+  "name": "felgi",
+  "description": "Chromowane felgi",
+  "amount": "10",
+  "price": "99"
+}]
+# other column settings -> http://bootstrap-table.wenzhixin.net.cn/documentation/#column-options
+columns = [
+  {
+    "field": "name", # which is the field's name of data key 
+    "title": "name", # display as the table header's name
+    "sortable": True,
+  },
+  {
+    "field": "description",
+    "title": "description",
+    "sortable": True,
+  },
+  {
+    "field": "amount",
+    "title": "amount",
+    "sortable": True,
+  },
+  {
+    "field": "price",
+    "title": "price",
+    "sortable": True,
+  }
+]
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Dashboard')
+
+@app.route('/course/<coursename>')
+def course(coursename):
+    """course = Course.query.filter_by(coursename=coursename).first_or_404()
+    course_id = course.id
+    files = Files.query.filter_by(course_id=course_id)"""
+    #return render_template('course.html', course=course, files=files)
+    return render_template('course.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html', downloadingText="dummy value")
+
+@app.route('/browse')
+def browse():
+    return render_template('browse.html')
+
+@app.route('/pending')
+def pending():
+    return render_template('pending.html')
+
+@app.route('/finished')
+def finished():
+    return render_template('finished.html')
+
+@app.route('/warehouse')
+def warehouse():
+    #return render_template("table.html", data=data, columns=columns, title='Flask Bootstrap Table')
+    return render_template('warehouse.html', data=data, columns=columns, title='Tabela')
+
+
+if __name__ == '__main__':
+	#print jdata
+  app.run(debug=True)
+
+
