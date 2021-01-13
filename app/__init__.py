17a59da22c7c48990b91849419ffbd9a1fb2b8f5
hbery-HYPERION <a.twardosz98@gmail.com>
2021-01-13 15:23:11 +0100

    conf mysql

--- app/__init__.py
+++ app/__init__.py
@@ -10,12 +10,14 @@ app.config.from_mapping(SECRET_WORD='dev')
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 mysql = None
 if platform.node() == 'bazy':
-    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_HOST'] = 'bazy.labagh.pl'
     app.config['MYSQL_USER'] = 'g02'
     app.config['MYSQL_PASSWORD'] = 'uythozxt'
     app.config['MYSQL_DB'] = 'g02'
 
     mysql = MySQL(app)
+
+
 bootstrap = Bootstrap(app)
 
 if os.name == 'nt':
