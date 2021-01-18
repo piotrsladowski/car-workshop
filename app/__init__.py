d22a47b1f9a13cdb7e1eb5fe736d2ec7efc2ceb9
egon <piotrek9880@gmail.com>
2021-01-18 18:37:14 +0100

    fixed error handling

--- app/__init__.py
+++ app/__init__.py
@@ -23,6 +23,9 @@ bootstrap = Bootstrap(app)
 
 if os.name == 'nt':
     from car_workshop.app import routes, errors
-else:    
+else:
     if not (subprocess.check_output('uname -mrs', stderr=subprocess.STDOUT, shell=True).rstrip().decode('utf-8') == 'Linux 4.18.0-240.1.1.el8_3.x86_64 x86_64'):
         from car_workshop.app import routes, errors
+    else:
+        from project.car_workshop.app import routes, errors
+
--- app/errors.py
+++ app/errors.py
@@ -4,15 +4,22 @@ import os
 import time
 
 if os.name == 'posix':
+  print("test")
   if (subprocess.check_output('uname -mrs', stderr=subprocess.STDOUT, shell=True).rstrip().decode('utf-8') == 'Linux 4.18.0-240.1.1.el8_3.x86_64 x86_64'):
     from project.car_workshop.app import app
   else:
-    from car_workshop.app import app, mysql
+    from car_workshop.app import app
 elif os.name == 'nt':
-  from car_workshop.app import app, mysql
+  from car_workshop.app import app
 
 
 @app.errorhandler(404)
 def not_found_error(error):
     num = int(time.time()) % 2
     return render_template('404.html', num=num), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
+
