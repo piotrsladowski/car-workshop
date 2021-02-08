61db96a13c08828e46f12ec3e8c9af6449a641ba
egon <piotrek9880@gmail.com>
2021-02-08 10:43:21 +0100

    from the other side im fixing

--- app/__init__.py
+++ app/__init__.py
@@ -9,13 +9,14 @@ app = Flask(__name__, instance_relative_config=True)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 mysql = MySQL()
 if platform.node() == 'bazy':
-    from project.config import Config
-    # app.config['MYSQL_HOST'] = 'localhost'
-    # app.config['MYSQL_USER'] = 'g02'
-    # app.config['MYSQL_PASSWORD'] = 'uythozxt'
-    # app.config['MYSQL_DB'] = 'g02'
-    # app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
-    app.config.from_object("Config")
+    from project import config
+    cnf = config.Config()
+    app.config.from_object(cnf)
+    app.config['MYSQL_HOST'] = cnf.DB_HOST
+    app.config['MYSQL_USER'] = cnf.DB_USER
+    app.config['MYSQL_PASSWORD'] = cnf.DB_PASSWORD
+    app.config['MYSQL_DB'] = cnf.DB_DB
+    app.config['MYSQL_CURSORCLASS'] = cnf.DB_CURSORCLASS
 
     mysql.init_app(app)
 
