958caf2801a10a588061c9df67998a1ec274b8be
hbery-HYPERION <a.twardosz98@gmail.com>
2021-02-08 11:48:57 +0100

    warehouse still fixing

--- app/routes.py
+++ app/routes.py
@@ -454,10 +454,7 @@ def warehouse():
     for row in rv:
       c.execute('''select (select cm.car_model as 'cm' from car_models as cm where cm.id=cp.car_id) from cars_parts as cp where cp.part_id={}'''.format(row['id']))
       models = c.fetchall()
-      ms = []
-      for m in models:
-        ms.append(m['cm'])
-      mds = ', '.join( ms )
+      mds = ', '.join( models )
       data.append({'code': row['code'], 'original': row['is_original'], 'price': row['price'], 'amount': row['amount'], 'models': mds, 'description': row['description']})
 
     columns = [
